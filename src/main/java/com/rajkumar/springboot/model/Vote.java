bb25f100cbaee0b6661cb942652e6ad18735128b
Rajkumar <myidrajkumar@gmail.com>
2022-06-28 12:01:31 +0530

    Handled Lombok super class equals handling

--- src/main/java/com/rajkumar/springboot/model/Vote.java
+++ src/main/java/com/rajkumar/springboot/model/Vote.java
@@ -1,20 +1,22 @@
 package com.rajkumar.springboot.model;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
 @Entity
 @Data
 @NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
 public class Vote extends Auditable {
-
+    
     @Id
     @GeneratedValue
     private Long id;
     private int vote;
-
+    
 }
